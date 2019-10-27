28b8b098929f86246d718bc15e6f96e93bebf77b
Tomdozz <tom_1995@msn.com>
2019-10-27 21:23:46 +0100

    Sass and start register/loginpage
    Project: Startup
    Task: N/A

---
+++ express/src/components/LoginRegister.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="loginform">
+      <h1>Login to Lifearray</h1>
+    </div>
+    <div class="registerform">
+      <i>Not registered yet?, register here.</i>
+      <div>
+        <input type="email" name="email" placeholder="email" v-model="registerEmail" />
+        <input type="password" name="password" placeholder="pass" v-model="registerPassword" />
+        <button @click="register">Register</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from "@/services/AuthenticationService";
+
+export default {
+  data() {
+    return {
+      registerEmail: "",
+      registerPassword: ""
+    };
+  },
+  methods: {
+    async register() {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      });
+      console.log(response.data);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "@/assets/sass/variables.scss";
+
+@media screen and(max-width:480px){
+  /*
+    Egenskaper för mobiler
+  */
+}
+
+@media screen and (min-width: 480px) and (max-width: 1024px){
+  /*
+    Egenskaper för tablets
+  */
+}
+
+@media screen and (min-width: 1025px){
+  /*
+    Egenskaper för desktop
+  */
+}
+</style>
--- express/src/router/index.js
+++ express/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
-import Register from '@/components/Register';
+import LoginRegister from '@/components/LoginRegister';
 
 
 Vue.use(Router);
@@ -14,9 +14,9 @@ export default new Router({
       component: HelloWorld,
     },
     {
-      path: '/register',
-      name: 'register',
-      component: Register,
+      path: '/login',
+      name: 'login',
+      component: LoginRegister,
     },
   ],
 });
