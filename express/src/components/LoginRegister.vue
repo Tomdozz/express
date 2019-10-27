dcf013b31f42c0785de65bf33ee8399a5c8fecf6
tomdozz <tom_1995@msn.com>
2019-10-27 23:04:17 +0100

    styled login/registerpage, next up validation
    Project:startup
    Task: N/A

--- express/src/components/LoginRegister.vue
+++ express/src/components/LoginRegister.vue
@@ -1,15 +1,71 @@
 <template>
-  <div>
-    <div class="loginform">
+  <div class="maincontent">
+    <div class="loginform" v-if="isLogin">
       <h1>Login to Lifearray</h1>
-    </div>
-    <div class="registerform">
-      <i>Not registered yet?, register here.</i>
-      <div>
-        <input type="email" name="email" placeholder="email" v-model="registerEmail" />
-        <input type="password" name="password" placeholder="pass" v-model="registerPassword" />
-        <button @click="register">Register</button>
+      <p>
+        Not registered yet?,
+        <a @click="displayRegister">register here</a>
+      </p>
+      <div class="inputs">
+        <input
+          class="standard-input"
+          type="email"
+          name="loginEmail"
+          placeholder="ex. yourname@gmail.com"
+          v-model="loginEmail"
+        />
       </div>
+      <div class="inputs">
+        <input
+          class="standard-input"
+          type="password"
+          name="loginPassword"
+          placeholder="***"
+          v-model="loginPassword"
+        />
+      </div>
+      <button class="button" @click="register">Login</button>
+      <p class="forgotPassword">Forgot your password? no worries, we help you!</p>
+    </div>
+    <div class="registerform" v-if="!isLogin">
+      <h1>Create account</h1>
+      <div class="inputs">
+        <input
+          class="standard-input"
+          type="name"
+          name="firstName"
+          placeholder="FirstName"
+          v-model="registerFirstName"
+        />
+      </div>
+      <div class="inputs">
+        <input
+          class="standard-input"
+          type="password"
+          name="lastName"
+          placeholder="LastName"
+          v-model="registerLasttName"
+        />
+      </div>
+      <div class="inputs">
+        <input
+          class="standard-input"
+          type="email"
+          name="email"
+          placeholder="Email Address"
+          v-model="registerEmail"
+        />
+      </div>
+      <div class="inputs">
+        <input
+          class="standard-input"
+          type="password"
+          name="password"
+          placeholder="Password"
+          v-model="registerPassword"
+        />
+      </div>
+      <button class="button" @click="register">Register</button>
     </div>
   </div>
 </template>
@@ -21,7 +77,12 @@ export default {
   data() {
     return {
       registerEmail: "",
-      registerPassword: ""
+      registerPassword: "",
+      loginEmail: "",
+      loginPassword: "",
+      registerFirstName: "",
+      registerLastName: "",
+      isLogin: true
     };
   },
   methods: {
@@ -31,28 +92,67 @@ export default {
         password: this.password
       });
       console.log(response.data);
+    },
+    displayRegister() {
+      this.isLogin = !this.isLogin;
+      console.log("display register");
     }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
-@import "@/assets/sass/variables.scss";
+<style lang="css" scoped>
+.maincontent {
+  position: fixed;
+  top: 50%;
+  left: 20%;
+  transform: translate(-0%, -50%);
+}
 
-@media screen and(max-width:480px){
+.inputs {
+  padding-top: 1em;
+  padding-bottom: 1em;
+}
+.standard-input {
+  border: 0;
+  outline: 0;
+  background: transparent;
+  border-bottom: 1px solid black;
+}
+
+.button {
+  background-color: grey;
+  border: none;
+  color: white;
+  padding: 10px 50px;
+  font-size: 12px;
+}
+p {
+  opacity: 0.4;
+}
+p a {
+  text-decoration: underline;
+  cursor: pointer;
+}
+
+.forgotPassword{
+  font-size: 12px;
+}
+@media screen and(max-width:480px) {
+  /* center main content */
   /*
     Egenskaper för mobiler
   */
 }
 
-@media screen and (min-width: 480px) and (max-width: 1024px){
+@media screen and (min-width: 480px) and (max-width: 1024px) {
   /*
     Egenskaper för tablets
   */
 }
 
-@media screen and (min-width: 1025px){
+@media screen and (min-width: 1025px) {
   /*
     Egenskaper för desktop
   */
